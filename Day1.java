9978f508ef92067ebba6d2c85550fce02aa33efd
mraniket404 <aniketgosavi471@gmail.com>
2025-10-22 19:00:05 +0530

    day 1 palindromes

---
+++ Day1.java
@@ -0,0 +1,33 @@
+class Day1 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;  // Negative numbers kabhi palindromes nhi hota
+        }
+
+        int n = x;   // Copy original number
+        int rev = 0; //  store the reversed number
+
+        // Reverse the number
+        while (n > 0) {
+            int d = n % 10;     // Extract last digit
+            rev = rev * 10 + d; // Build reversed number
+            n = n / 10;         // Remove last digit
+        }
+
+        // Compare reversed and original numbers
+        return rev == x;
+    }
+
+
+//    for testing 
+    public static void main(String[] args) {
+        Day1 obj = new Day1();
+
+      
+        System.out.println(obj.isPalindrome(121));  
+        System.out.println(obj.isPalindrome(-121)); 
+        System.out.println(obj.isPalindrome(10));   
+        System.out.println(obj.isPalindrome(1221)); 
+        System.out.println(obj.isPalindrome(12345));
+    }
+}
