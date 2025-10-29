26bb89bc037493b1020ebb2e2493d4cb8efdf8cb
mraniket404 <aniketgosavi471@gmail.com>
2025-10-29 10:46:34 +0530

    Day 8

--- Day8.java
+++ Day8.java
@@ -1,5 +1,5 @@
 //Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
-
+// Day8
 import java.util.*;
 
 public class Day8 {
