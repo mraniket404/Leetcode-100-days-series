b276db1b42f81f16242e3bae9f07b8b58b764b38
mraniket404 <aniketgosavi471@gmail.com>
2025-10-31 10:07:07 +0530

    Day10

---
+++ Day10.java
@@ -0,0 +1,26 @@
+class Day10 {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSum = (range * (range + 1)) / 2;
+        
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+        }
+        
+        return actualSum - currentSum;
+    }
+    
+    public static void main(String[] args) {
+        Day10 solution = new Day10();
+        
+        // Test cases
+        int[] nums1 = {3, 0, 1};
+        int[] nums2 = {0, 1};
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        
+        System.out.println("Missing number in [3, 0, 1]: " + solution.missingNumber(nums1));
+        System.out.println("Missing number in [0, 1]: " + solution.missingNumber(nums2));
+        System.out.println("Missing number in [9, 6, 4, 2, 3, 5, 7, 0, 1]: " + solution.missingNumber(nums3));
+    }
+}
