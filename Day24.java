ef5ee3d1717d087bf9fb37dde73f97a58441929c
mraniket404 <aniketgosavi471@gmail.com>
2025-11-14 20:26:26 +0530

    Day24

---
+++ Day24.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Day24 {
+
+    public static int findMin(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        } else if (nums[0] < nums[nums.length - 1]) {
+            return nums[0];
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (mid != 0 && nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            } else if (mid != nums.length - 1 && nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            } else if (nums[start] <= nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,5,6,7,0,1,2};
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum element is: " + result);
+    }
+}
