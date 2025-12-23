c6e650e917de92e67276c3f323583af6a8daa2a1
mraniket404 <aniketgosavi471@gmail.com>
2025-12-23 18:04:11 +0530

    Day59

---
+++ Questions/Day59.java
@@ -0,0 +1,30 @@
+public class Day59 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Day59 obj = new Day59();
+
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+
+        System.out.println(obj.searchMatrix(matrix, 3));   // true
+        System.out.println(obj.searchMatrix(matrix, 13));  // false
+    }
+}
